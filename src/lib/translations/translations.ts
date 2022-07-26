2f3fa097c5bd818a0bc51c39c307c75df5091e58
geryy <g@geryy.io>
2022-07-26 23:14:10 +0200

    adds es-ES translation

--- src/lib/translations/translations.ts
+++ src/lib/translations/translations.ts
@@ -176,87 +176,88 @@ export default {
 	},
 	es: {
 		lang,
-		change_language: 'Seitensprache ändern',
+		change_language: 'Cambiar de idioma',
 		sunSquare: 'SunSquare',
-		impressions: 'Neuste Projekte',
-		section_headline: 'Sonne & Schatten<br/>365 Tage im Jahr.',
-		next: 'Nächstes',
-		prev: 'Zurück',
-		imgAlt: 'Rectangular Sails Premium Sails for Sun Protection',
+		impressions: 'Últimos proyectos',
+		section_headline: 'Sol y sombra<br/>365 días al año',
+		next: 'Siguiente',
+		prev: 'Volver',
+		imgAlt: 'Velas rectangulares Velas Premium para protección solar',
 		button: {
-			explore_sails: 'Zu den Modellen',
-			explore_triangular: 'Zu den dreieckigen  Modellen',
-			explore_rectengular: 'Zu den rechteckigen Modellen',
-			twelve_reasons: '12 Gründe für SunSquare'
+			explore_sails: 'Explorar velas',
+			explore_triangular: 'Explorar velas triangulares',
+			explore_rectengular: 'Explorar velas rectangulares',
+			twelve_reasons: '12 razones para SunSquare'
 		},
 		home: {
-			title: 'Premium-Segel<br>für Sonnenschutz & Ambiente',
+			title: 'Velas Premium<br>para proteger del sol y ambientar',
 			intro:
-				'Incompar designed seit 1999 Schatten auf den Balearischen Inseln, dem spanischen Festland sowie auf den Kanarischen Inseln. Egal ob Sie genaue Vorstellungen von ihrem Segel haben, wo Sie Schatten benötigen, oder sich absolut nicht sicher sind, wie Sie den von Ihnen gewählten Standort beschatten können. Wir sind ihre Fachfirma dafür.Wir beraten Sie vor Ort und erstellen gratis ein Festpreisangebot mit 3D Visualisierung und Schattenverlauf.',
-			how_title: 'WIE ES FUNKTIONIERT',
+				'Desde 1999, Incompar diseña sombra en las Islas Baleares, la Península y las Islas Canarias. Tanto si tiene una idea precisa sobre su vela, como si no está seguro de cómo sombrear el lugar elegido, somos sus expertos. Le asesoramos in situ y elaboramos una oferta con un precio fijo que incluye una visualización en 3D y la progresión de la sombra de forma gratuita.',
+			how_title: 'Cómo funciona. ',
 			how_content:
-				'Unser Fachpersonal installiert seit mehr als 25 Jahren Sonnensegel. Jede Anlage wird individuell auf die Anforderungen des Kunden und den örtlichen Gegebenheiten zugeschnitten.<br><br>Sie können sich darauf verlassen, dass wir das gesamte Projekt erstellen oder auf Ihre spezifischen Bedürfnisse eingehen.<br><br>Darüber hinaus erbringen wir anspruchsvolle Ingenieurleistungen, die notwendig sind, um den immer anspruchsvolleren und komplexeren Herausforderungen des modernen Baus gerecht zu werden.<br><br>Hausdämmung, Isolation, Arbeitssicherheit, Sauberkeit, Pünktlichkeit, Flexibilität und professionelles auftreten gehören bei uns zum Standard.'
+				'Nuestro personal especializado lleva más de 25 años instalando velas solares de alta gama. Cada sistema se adapta individualmente a las condiciones locales y a los requisitos del cliente.<br><br>Puede contar con nosotros para crear el proyecto completo o para atender sus necesidades específicas. Además, ofrecemos sofisticados servicios de ingeniería, necesarios para responder a los retos cada vez más exigentes y complejos de la construcción contemporánea.<br><br>El aislamiento de la vivienda, la seguridad laboral, la limpieza, la puntualidad, la flexibilidad y un aspecto profesional son para nosotros un estándar.<br>'
 		},
 		sails: {
-			models: 'Modelle',
-			catchFrase: 'Sonne & Schatten, 365 Tage pro Jahr.',
+			models: 'Modelos',
+			catchFrase: 'Sol y sombra. 365 días al año.',
 			triangular: {
-				premium: 'Premium<br/>Dreiecksegel',
-				name: 'Dreieckige ',
+				premium: 'VELAS TRIANGULARES PREMIUM.',
+				name: 'TRIANGULARES  ',
 				description:
-					'Mit der patentierten Technik von SunSquare ist das Dreiecksegel das ultimative Produkt in der Sonnensegel- und Beschattungstechnologie. Ein elektronisch automatisiertes  Rollsystem, maßgeschneidert für jeden Kunden, unter Verwendung der hochwertigsten verfügbaren Edelstahl- und Aluminiummaterialien; das Dreiecksegel ist die wahre Definition für Schattengebung. Incompar kann für Sie dieses unglaublich vielseitige Produkt als architektonischen Akzent, Hofüberdachung oder sogar als Freiflächenüberdachung bis zu 80qm2 pro Segel installieren. Das Dreieckssegel bietet Schatten, der Ihren Bedürfnissen entspricht.',
+					'Utilizando la ingeniería patentada de SunSquare, la vela triangular es el último logro en tecnología de velas solares y sombreado. Un sistema de enrollado electrónico construido a medida según el tamaño y las necesidades de cada cliente, utilizando los mejores materiales de acero inoxidable y aluminio del mercado. La vela triangular es la verdadera definición de sombra evolutiva.<br><br>Este producto increíblemente versátil puede instalarse como complemento arquitectónico, como cubierta de patio o incluso como cubierta de espacios abiertos de hasta 80 metros cuadrados por vela. ',
 				content:
-					'Mit der patentierten Technik von SunSquare ist das Dreiecksegel das ultimative Produkt in der Sonnensegel- und Beschattungstechnologie. Ein elektronisch automatisiertes  Rollsystem, maßgeschneidert für jeden Kunden, unter Verwendung der hochwertigsten verfügbaren Edelstahl- und Aluminiummaterialien; das Dreiecksegel ist die wahre Definition für Schattengebung.<br><br>Incompar kann für Sie dieses unglaublich vielseitige Produkt als architektonischen Akzent, Hofüberdachung oder sogar als Freiflächenüberdachung bis zu 80qm2 pro Segel installieren.Das Dreieckssegel bietet Schatten, der Ihren Bedürfnissen entspricht. Sollte sich ein Sonnensegel als unpassend erweisen, kann die gleiche Konstruktion für ein Doppeldreiecksegel verwendet werden, was eine alternative Positionierung der zentralen Spindel ermöglicht.Jedes Sonnensegel ist ein Unikat und wird nur für Sie produziert und passgenau bei ihrem Projekt montiert.'
+					'Utilizando la ingeniería patentada de SunSquare, la vela triangular es el último logro en tecnología de velas solares y sombreado. Un sistema de enrollado electrónico construido a medida según el tamaño y las necesidades de cada cliente, utilizando los mejores materiales de acero inoxidable y aluminio del mercado. La vela triangular es la verdadera definición de sombra evolutiva.<br><br>Este producto increíblemente versátil puede instalarse como complemento arquitectónico, como cubierta de patio o incluso como cubierta de espacios abiertos de hasta 80 metros cuadrados por vela. La vela triangular proporciona una sombra que satisface sus necesidades, pero si una vela solar resulta inadecuada, se puede utilizar la misma construcción para construir una vela triangular doble, lo que permite una colocación alternativa del eje central.<br>Cada toldo es único, fabricado especialmente para usted y adaptado a su proyecto.'
 			},
 			rectangular: {
-				premium: 'Premium<br/>Rechtecksegel',
-				name: 'Rechteckige',
+				premium: 'VELAS RECTANGULARES PREMIUM.',
+				name: 'RECTANGULARES ',
 				description:
-					'SunSquare hat erkannt, dass bei vielen modernen Bauten Dreiecksegel nicht brauchbar sind und hat darum das fortschrittlichste rechteckige Sonnensegel-System auf dem Markt entwickelt. Das Rechtecksegel, das seinen Ruf mit hochwertigstem Material für praktisch unverwüstliche Produkte aufrechterhält, ist genauso beeindruckend wie seine Geschwisterprodukte. Maßgeschneidert für die Bedürfnisse jedes Kunden, wird dieses elektronisch automatisierte Sonnensegel aus hochwertigem Edelstahl und Aluminiumteilen in Flugzeugqualität hergestellt.',
+					'SunSquare se dio cuenta de que muchos edificios modernos no pueden albergar velas triangulares, por lo que desarrolló el sistema de toldo rectangular más avanzado del mercado actual. Con su reputación de materiales de alta calidad y productos casi invencibles, el toldo rectangular no es menos impresionante que sus hermanos. Estos toldos solares automatizados electrónicamente están construidos con acero inoxidable y aluminio de calidad aeronáutica, hechos a medida para adaptarse a las necesidades de cada cliente. ',
 				content:
-					'SunSquare hat erkannt, dass bei vielen modernen Bauten Dreiecksegel nicht brauchbar sind und hat darum das fortschrittlichste rechteckige Sonnensegel-System auf dem Markt entwickelt. Das Rechtecksegel, das seinen Ruf mit hochwertigstem Material für praktisch unverwüstliche Produkte aufrechterhält, ist genauso beeindruckend wie seine Geschwisterprodukte. Maßgeschneidert für die Bedürfnisse jedes Kunden, wird dieses elektronisch automatisierte Sonnensegel aus hochwertigem Edelstahl und Aluminiumteilen in Flugzeugqualität hergestellt. Standard ist zudem auch ein Windwächter sowie eine Funkfernbedienung.<br><br>Erhältlich mit Stoffen, die einem gleichermaßen Regen- und Sonnenschutz bieten, kann das Design für jede Größe und jeden Bedarf angepasst werden, von Restauranthöfen über private Pool- Lounge - Bereiche bis hin zu Bootsdecks.<br><br>Ebenso halten die Segel Abendfeuchte ab und es entsteht ein Raumgefühl welches nachts mit indirekter Beleuchtung ins Segeltuch noch verstärkt wird.Auch hierzu hat Sunsquare LED Leuchten entwickelt die perfekt zu den Segeln passen.Sie sind dimmbar und über Funksteuerung zu bedienen.'
+					'SunSquare se dio cuenta de que muchos edificios modernos no pueden albergar velas triangulares, por lo que desarrolló el sistema de toldo rectangular más avanzado del mercado actual. Con su reputación de materiales de alta calidad y productos casi invencibles, el toldo rectangular no es menos impresionante que sus hermanos. Estos toldos solares automatizados electrónicamente están construidos con acero inoxidable y aluminio de calidad aeronáutica, hechos a medida para adaptarse a las necesidades de cada cliente. Como característica estándar, se proporciona un monitor de viento y un control remoto por radio.<br><br>Disponible en tejidos que ofrecen igual protección contra la lluvia y el sol, este producto increíblemente versátil puede instalarse como complemento arquitectónico, como cubierta de patio, en cubiertas de barco o incluso como cubierta de espacios abiertos.<br>Además, las velas impiden que entre la humedad de la tarde y crean una sensación de espacio que se ve reforzada por la iluminación indirecta dentro de la lona por la noche. Sunsquare ha desarrollado luces LED regulables y de radiocontrol que se adaptan perfectamente a las velas.'
 			}
 		},
 		about: {
-			title: 'Incompar, Ihr führender Anbieter von Sonnensegeln seit 1999. ',
+			title: 'Incompar, Líder en velas de sol desde 1999.<br>',
 			subtitle:
-				'Incompar steht für mehr als 50 Jahre Erfahrung mit Sonnen-, und Regenschutz. Mithilfe einzigartiger Designs und modernster 3D Technologie verwandeln wir Traum- Immobilien in etwas außergewöhnliches.',
-			key_facts: 'Fakten',
-			sales: 'Verkaufsvolumen',
-			years: 'Jahre',
-			projectsAnno: 'Projekte pro Jahr',
-			experience: 'Jahre Erfahrung',
-			values_title: 'Unsere Werte',
+				'En Incompar nos hemos consolidado en el mercado como el principal proveedor de velas galardonadas de alta gama, velas especiales y soluciones de sombreado de cualquier tamaño y forma.',
+			key_facts: 'Datos clave',
+			sales: 'en volumen de ventas',
+			years: 'año',
+			projectsAnno: 'proyectos al año',
+			experience: 'años de experiencia',
+			values_title: 'Nuestros valores',
 			values: {
-				SERVICE_TITLE: 'Kundenservice',
+				SERVICE_TITLE: 'SERVICIO AL CLIENTE',
 				SERVICE:
-					'Zuverlässigkeit, Flexibilität, Fachkompetenz und handwerkliches Geschick sind selbstverständlich für uns. In Kombination mit unserem Engagement und unserer Stärke als Team sehen wir jede Aufgabe als Herausforderung. Die Zufriedenheit jedes einzelnen Kunden hat für uns höchste Priorität.',
-				HOTEL_TITLE: 'HOTELS, RESTAURANTS & MEHR',
+					'La fiabilidad, la flexibilidad, la experiencia y la artesanía son evidentes para nosotros. En combinación con nuestro compromiso y nuestra fuerza como equipo, vemos cada trabajo como un reto. La satisfacción de cada cliente es nuestra máxima prioridad.',
+				HOTEL_TITLE: 'HOTELES, RESTAURANTES Y MÁS',
 				HOTEL:
-					'Sonnen- und Regen-Lösungen für Hotels, Apartments, Restaurants, Villen und Großprojekte sind unser tägliches Geschäft, aber niemals Routine.',
-				PLANNING_TITLE: '3D-PLANUNG',
+					'Ofrecemos soluciones para sol y lluvia a diario para hoteles, apartamentos, restaurantes, villas y proyectos de gran envergadura sin caer en la rutina.',
+				PLANNING_TITLE: 'PLANNING 3D',
 				PLANNING:
-					'Wir entwickeln für Sie einzigartige Designs, produzieren alles aus unseren interaktiven 3D-Modellen und arbeiten mit architektonischen Grundrissen, selbst wenn Ihr Haus noch nicht gebaut sein sollte.',
-				ARCHITECTURE_TITLE: 'ARCHITEKTUR',
+					'Desarrollamos diseños únicos y producimos todo a partir de nuestros modelos interactivos en 3D, trabajando con planos arquitectónicos incluso cuando la propiedad aún no está construida.',
+				ARCHITECTURE_TITLE: 'ARQUITECTURA',
 				ARCHITECTURE:
-					'Durch die Kombination unserer Fähigkeiten und unseres Fachwissens in den Bereichen Architektur, Design und Planung können wir unseren Kunden Räume bieten, die sowohl funktional als auch effizient sind und es ihnen ermöglichen, das Leben im Freien noch mehr zu genießen.'
+					'Combinando nuestras habilidades y experiencia en arquitectura, diseño y planificación podemos proporcionar a nuestros clientes espacios que son funcionales y eficientes, permitiéndoles disfrutar aún más de la vida al aire libre.'
 			},
-			content: `<p>Wir von Incompar, premium Partner Sunquare Spanien,  haben uns als Anbieter von preisgekrönten High-End-Sonnensegeln, Spezialmarkisen und individuellen Beschattungslösungen in jeder Größe und Form am Markt etabliert.</p><p>Als Top 3 Partner von SunSquare genießt Incompar seit mehr als 20 Jahren das Vertrauen seiner Partner und Kunden für die hohe Qualität seiner Produkte, Installationen und Dienstleistungen auf den Balearen, dem spanischen Festland und den Kanarischen Inseln.  Wir sind spezialisiert auf Großprojekte, Hotels, Privatgrundstücke und Restaurants.</p><p>Beginnend mit einer unverbindlichen Beratung und einem anschließenden professionellen Aufbau ist unser oberstes Ziel, eine gute und langfristige Beziehung zu unseren Kunden aufzubauen. Unsere Anlagen aus den ersten Firmenjahren laufen immer noch einwandfrei!</p><p>Incompar steht für hochwertige, individuelle Design- und Architekturlösungen für unsere Kunden.</p><p>Rufen Sie uns jetzt an, um einen Termin für eine kostenlose Beratung mit erfahrenen Sunsquare-Mitarbeitern zu vereinbaren, um schnell festzustellen, wie Sie am besten ein Sonnensegel auf Ihrem Grundstück installieren können.</p>`
+			content:
+				'<p>Incompar lleva más de 20 años ofreciendo protección contra el sol y la lluvia. Con la ayuda de diseños únicos y la última tecnología 3D, transformamos propiedades de ensueño en algo extraordinario.</p><p>Como socio premium de SunSquare España, Incompar goza de la confianza de sus socios y clientes por la alta calidad de sus productos, instalaciones y servicios en las Islas Baleares, la Península y las Islas Canarias, desde hace más de 20 años. Estamos especializados en proyectos de gran envergadura, hoteles, propiedades privadas y restaurantes.</p><p>Desde el inicio de nuestra consulta sin compromiso hasta el final de nuestra instalación profesional, nuestro principal objetivo es construir una relación a largo plazo con nuestros clientes. Los sistemas siguen funcionando a la perfección desde el primer año.</p><p>Incompar es sinónimo de alta calidad, diseño personalizado y soluciones arquitectónicas para nuestros clientes.</p><p>Llámenos ahora para concertar una cita para una consulta gratuita con el personal experimentado de Sunsquare para determinar la mejor manera de instalar una vela solar en su propiedad.</p>'
 		},
 		reasons: {
-			title: '12 GUTE GRÜNDE FÜR EIN ORIGINAL SUNSQUARE® SONNENSEGEL'
+			title: '12 razones para Sunsquare'
 		},
 		contact: {
-			general: 'Allgemeines',
-			costumer_service: 'Kundenservice',
-			for_professionals: 'Für Professionelle',
-			office_hours: 'Öffnungszeiten',
-			days: 'Montag - Freitag',
-			show_map: 'Karte anzeigen'
+			general: 'General',
+			costumer_service: 'Atención al cliente',
+			for_professionals: 'Para profesionales',
+			office_hours: 'Horario de oficina',
+			days: 'Lunes a viernes de',
+			show_map: 'Mostrar mapa'
 		},
 		models: {
 			intro:
-				'SunSquare erweitert permanent seine Produktlinie und führt das originale SunSquare Sonnensegel neuen Interpretationen zu. Die neue Produktgeneration wird mit dem Zusatz „Cantilever“ im Produktnamen angeführt. Die neuerste Errungenschaft - der Luxus-Segelschirm FOLD & ROLL. Dieser wurde auf diversen Messen dem fachkundigen Publikum präsentiert und erregte viel Aufmerksamkeit und internationales Echo.'
+				'SunSquare está ampliando constantemente su línea de productos, llevando el toldo original SunSquare a nuevas interpretaciones. La nueva generación de productos se cita con la adición "Cantilever" en nombre del producto. El último logro - la lujosa vela FOLD & ROLL, se presentó a un público experto en varias ferias y despertó mucha atención y respuesta internacional.'
 		}
 	}
 };
