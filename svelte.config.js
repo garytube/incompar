971afb7f7eaa9b16d4d74c80bc153a21207d2656
geryy <g@geryy.io>
2022-07-28 11:48:09 +0200

    cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
-import path from 'path';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [preprocess()],
