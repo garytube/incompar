04e25d9ec70816c421924b4f4c543df0cf2effed
geryy <g@geryy.io>
2022-07-28 11:49:02 +0200

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -8,50 +8,38 @@ import path from 'path';
 
 const today = new Date().toLocaleDateString();
 
-// /** @type {import('vite').UserConfig} */
-// const config = {
-// 	plugins: [
-// 		sveltekit(),
-// 		banner('Generated incopmpar.es:' + today),
-// 		viteImagemin({
-// 			optipng: {
-// 				optimizationLevel: 7
-// 			},
-// 			mozjpeg: {
-// 				progressive: true,
-// 				quality: 70,
-// 				quantTable: 3,
-// 				smooth: 60
-// 			},
-// 			pngquant: {
-// 				quality: [0.8, 0.9],
-// 				speed: 4
-// 			}
-// 		}),
-// 		ViteWebfontDownload([
-// 			'https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;400;700;900&display=swap'
-// 			// 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap'
-// 		])
-// 	],
-// 	resolve: {
-// 		alias: {
-// 			$assets: path.resolve('src/assets')
-// 		}
-// 	},
-// 	server: {
-// 		port: 3000
-// 	}
-// };
-
-import path from 'path';
-
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		banner('Generated incopmpar.es:' + today),
+		viteImagemin({
+			optipng: {
+				optimizationLevel: 7
+			},
+			mozjpeg: {
+				progressive: true,
+				quality: 70,
+				quantTable: 3,
+				smooth: 60
+			},
+			pngquant: {
+				quality: [0.8, 0.9],
+				speed: 4
+			}
+		}),
+		ViteWebfontDownload([
+			'https://fonts.googleapis.com/css2?family=Heebo:wght@100;200;400;700;900&display=swap'
+			// 'https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap'
+		])
+	],
 	resolve: {
 		alias: {
 			$assets: path.resolve('src/assets')
 		}
+	},
+	server: {
+		port: 3000
 	}
 };
 
